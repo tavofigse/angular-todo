1e960bd33a3757a002b2ba5826f2842f2a278bdd
Gustavo <gfasequeira@gmail.com>
2016-01-30 16:29:48 -0300

    Fix in files

--- dist/data.js
+++ dist/data.js
@@ -1,24 +1,26 @@
 'use strict';
 
-angular.module('todoListApp')
-.service('dataService', function($http) {
+var todoListApp = angular.module('todoListApp');
+
+
+todoListApp.service('dataService', ['$http', function($http) {
   this.helloWorld = function() {
     console.log("This is the data service's method!!");
   };
-  
+
   this.getTodos = function(callback){
     $http.get('mock/todos.json')
     .then(callback)
   };
-  
+
   this.deleteTodo = function(todo) {
     console.log("The " + todo.name + " todo has been deleted!")
     // other logic
   };
-  
+
   this.saveTodo = function(todo) {
     console.log("The " + todo.name + " todo has been saved!");
     // other logic...
   };
-  
-});
+
+}]);
